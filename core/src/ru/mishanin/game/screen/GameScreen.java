9210bfff72745ed72f8c492fff0922cba527a113
AlekseyMishanin <mishanin_a_a@mail.ru>
2019-01-23 00:21:49 +0300

    Выполнено ДЗ в рамках 3-го урока.

--- core/src/ru/mishanin/game/screen/GameScreen.java
+++ core/src/ru/mishanin/game/screen/GameScreen.java
@@ -1,6 +1,7 @@
 package ru.mishanin.game.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import ru.mishanin.game.MyFirstGame;
@@ -12,35 +13,41 @@ import ru.mishanin.game.base.Base2DScreen;
  * */
 public class GameScreen extends Base2DScreen {
 
-    private MyFirstGame game;   //ссылка на объект типа MyFirstGame
     private Vector2 pos;        //координаты левого нижнего угла игрового объекта
     private Vector2 speed;      //скорость перемещения игрового объекта
+    private Vector2 buf;        //временный буфер
     private Texture img;        //текстура игрового объекта
     private Texture fon;        //текстура фона
+    private Vector2 endpoint;         //координаты конечной точки
+    private float step = 0.02f;            //шаг движения при нажатии клавишы направления
+    private static float V_SPEED = 0.0005f;     //скорость
+    private boolean bl1;                        //флаг для оптимизации работы метода render
 
     /**
      * Конструктор
      * @param game - ссылка на объект типа MyFirstGame
      * */
     public GameScreen(MyFirstGame game) {
-        this.game = game;
+
+        super(game);
+        this.endpoint = new Vector2();
+        img = new Texture("badlogic.jpg");
+        fon = new Texture("gameFon.jpg");
+
+        pos = new Vector2(-0.5f,-0.5f);
+        speed = new Vector2(0.001f,0.001f);
+        buf = new Vector2(0f,0f);
+
+        bl1=false;
     }
 
     @Override
     public void show() {
         super.show();
 
-        img = new Texture("badlogic.jpg");
-        fon = new Texture("gameFon.jpg");
-
-        pos = new Vector2(0f,0f);
-        speed = new Vector2(0f,0f);
-
-        rectangle.setSize(img.getWidth(),img.getHeight());  //сохраняем размеры игрового объекта
-
         game.setBackgroundSprite(fon);                      //загружаем текстуру для нового фона
-        game.getBackgroundSprite().setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        game.getBackgroundSprite().setPosition(0f,0f);
+        game.getBackgroundSprite().setSize(1f, 1f);
+        game.getBackgroundSprite().setPosition(-0.5f,-0.5f);
     }
 
     @Override
@@ -48,24 +55,25 @@ public class GameScreen extends Base2DScreen {
         super.render(delta);
         game.getBatch().begin();
         game.getBackgroundSprite().draw(game.getBatch());
-        game.getBatch().draw(img, pos.x, pos.y);
+        game.getBatch().draw(img, pos.x, pos.y, 0.5f,0.5f);
         game.getBatch().end();
-        /* В родительском классе Base2DScreen реализована булевая переменная для подтверждения факта нажатия пальцем/мышкой
-        по экрану или клавишы направления. Булевая переменная и нижесодержащаяся проверка введены, чтобы минимизировать
-        кол-во холостых математических операций с векторами.
-        * */
-        if(isFlagNewTouch()){
-            speed.set(endpoint);    //присваиваем вектору скорости координаты новой конечной точки
-            speed.sub(pos);         //определяем новое значение скорости
-            speed.nor();            //нормируем вектор скорости, чтобы избежать мгновенного перемещения игрового объекта в конечную точку
-            setFlagNewTouch(false); //обновляем булевую переменнюу родительского класса
+        if(!bl1){       //проверка, чтобы избежать холостых расчетов и лишнего нагрузки на ЦП
+            buf.set(endpoint);
+            if(buf.sub(pos).len2()<(V_SPEED)){
+                pos.set(endpoint);
+                bl1=true;
+            }
+            else{
+                pos.add(speed);
+            }
         }
-        //если округленные координаты х/у игрового объекта равны х/у конечной точки, то пропускаем данный шаг
-        if(Math.round(pos.x)==Math.round(endpoint.x)&&Math.round(pos.y)==Math.round(endpoint.y)) {}
-        else{
-            //иначе прибавляем к вектору положения игрового объекта вектор скорости
-            pos.add(speed);
-        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        System.out.println(width+" "+height);
+        System.out.println(Gdx.graphics.getWidth()+" "+Gdx.graphics.getHeight());
     }
 
     @Override
@@ -73,5 +81,56 @@ public class GameScreen extends Base2DScreen {
         img.dispose();
         fon.dispose();
         super.dispose();
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode){
+            case Input.Keys.LEFT:
+                //если координата х конечной точки равна -0.5 - завершаем обработку
+                if(endpoint.x==-0.5) {break;}
+                //перемещаем игровой объекта на шаг влево
+                else if(endpoint.x<0) {endpoint.x-=((0.5f+endpoint.x)< step ? (0.5f+endpoint.x) : step);}
+                else if(endpoint.x>=0) {endpoint.x-=step;}
+                updatePosEndVector2();
+                break;
+            case Input.Keys.RIGHT:
+                //если координата (х+ширина игрового объета) конечной точки равна ширине игрового экрана в мировых координатах - завершаем обработку
+                if(endpoint.x==0.5f) break;
+                //перемещаем игровой объекта на шаг вправо
+                else if(endpoint.x<0) {endpoint.x+=((endpoint.x+step)<0.5f ? step:0.5f-endpoint.x);}
+                else if(endpoint.x>=0) {endpoint.x=0.0f;}
+                updatePosEndVector2();
+                break;
+            case Input.Keys.UP:
+                //если координата (у+высота игрового объета) конечной точки равна высоте игрового экрана в мировых координатах - завершаем обработку
+                if(endpoint.y==0.5f) break;
+                //перемещаем игровой объекта на шаг вверх
+                else if(endpoint.y<0) {endpoint.y+=((endpoint.y+step)<0.5f ? step:0.5f-endpoint.y);}
+                else if(endpoint.y>=0) {endpoint.y=0.0f;}
+                updatePosEndVector2();
+                break;
+            case Input.Keys.DOWN:
+                //если координата у конечной точки равна -0.5 - завершаем обработку
+                if(endpoint.y==-0.5) break;
+                //перемещаем игровой объекта на шаг вниз
+                else if(endpoint.y<0) {endpoint.y-=((0.5f+endpoint.y)< step ? (0.5f+endpoint.y) : step);}
+                else if(endpoint.y>=0) {endpoint.y-=step;}
+                updatePosEndVector2();
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        endpoint.set(screenX,screenBounds.getHeight()-screenY).mul(screenToWorlds);
+        updatePosEndVector2();
+        return false;
+    }
+
+    private void updatePosEndVector2(){
+        speed.set(endpoint).sub(pos).setLength2(speed.len2()*V_SPEED);        //присваиваем новое значение скорости
+        bl1=false;
     }
 }
